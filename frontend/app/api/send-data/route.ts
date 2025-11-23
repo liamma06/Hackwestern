d99ec23c61bd15951df2e4ec7380e7dffbf1d017
liamma06 <liam.jbr.ma@gmail.com>
2025-11-22 23:31:00 -0500

    route

--- frontend/app/api/send-data/route.ts
+++ frontend/app/api/send-data/route.ts
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  // You can process or store the data here if needed
-
-  // Just echo the received data back
+  const status = await req.json(); // status is true or false
+  const value = status ? 1 : 0;
   return NextResponse.json({
     status: 'success',
-    received: data,
+    received: value
   });
 }
